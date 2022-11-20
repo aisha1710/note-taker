1b96cf79b173890f97a8445d6e92d85d66f23a34
aisha <faisha879@gmail.com>
2022-11-20 21:34:31 +0000

    committing post route

--- server.js
+++ server.js
@@ -20,3 +20,15 @@ app.get("/api/notes", (req, res) => {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
+
+app.post("/api/notes", (req, res) => {
+  let noteArray = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+  req.body.id = noteArray.length.toString();
+  noteArray.push(req.body);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(noteArray));
+  res.json(noteArray);
+});
+
+app.listen(PORT, () => console.log("Server listening on port " + PORT));
